30e5cc56f75576cf05f28e62ae4d8d1d25178eb5
serifahmetharas <serifahmetharas@gmail.com>
2022-01-21 17:02:43 +0300

    Singleton Design Pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+
+            return _customerManager ?? (_customerManager = new CustomerManager());
+
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+        }
+    }
+} 
