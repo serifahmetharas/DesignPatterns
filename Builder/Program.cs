7a9a59dfe8fe15839b95e93ed454f7ac622b1e4d
serifahmetharas <serifahmetharas@gmail.com>
2022-01-24 19:11:18 +0300

    Builder Design and Facade Design
    Builder and Facade designs are added.

---
+++ Builder/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Builder
+{
+    // Ortaya bir nesne örneği çıkarmayı hedefler. Bu nesne örneği birbiri ardına atılacak adımların işlenmesi ile ortaya çıkar.
+    // Genellikle iş katmanları ve arayüz katmanlarında sürekli if ler kullanmak yerine,
+    // İlgili üreticinin enjekte edilmesi ve ona göre ortaya bir nesne çıkarılması şeklinde örneklendiririz.
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirector director = new ProductDirector();
+            var builder = new NewCustomerProductBuilder();
+            director.GenerateProduct(builder);
+            var model = builder.GetModel();
+            
+            // Müşteri tipine göre ürünün yapısını oluşturduk, GetModel ile getirdik ve istediğimiz şekilde kullanabiliriz.
+            // If kullanımını engelleyen bir tasarım oluştu. 
+
+            Console.WriteLine(model.Id);
+            Console.WriteLine(model.CategoryName);
+            Console.WriteLine(model.DiscountApplied);
+            Console.WriteLine(model.DiscountedPrice);
+            Console.WriteLine(model.ProductName);
+            Console.WriteLine(model.UnitPrice);
+
+            Console.ReadLine();
+        }
+    }
+
+    class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public bool DiscountApplied { get; set; }
+    }
+
+    abstract class ProductBuilder
+    {
+        public abstract void GetProductData();
+        public abstract void ApplyDiscount();
+        public abstract ProductViewModel GetModel();
+        
+    }
+
+    class NewCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice*(decimal)0.90;  
+            model.DiscountApplied = true; // Yeni müşterilere indirim sağlama.
+        }
+
+
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverages";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+
+        }
+        public override ProductViewModel GetModel()
+        {
+            return model; // Ürettiğimiz modeli döndüren method.
+        }
+    }
+
+    class OldCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice= model.UnitPrice;
+            model.DiscountApplied = false;
+        }
+
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverages";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+
+        }
+        public override ProductViewModel GetModel()
+        {
+            return model; // Ürettiğimiz modeli döndüren method.
+        }
+    }
+
+    class ProductDirector
+    {
+        public void GenerateProduct(ProductBuilder productBuilder)
+        {
+            productBuilder.GetProductData();
+            productBuilder.ApplyDiscount();
+
+        }
+    }
+
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Facade
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save(); // İçinde Logging/Caching/Authorize işlemlerinin olduğu Interfaceleri tek class içinde newlediğimiz bir method.
+                                    // Ana programda gelip direk bu şekilde kullanabiliyoruz.
+            
+            // Artık bu sayede yaptıracağımız işlemler arttıkça interface ekleyerek, onu da class içine tanımlayarak,
+            // O class içinde istediğimiz methoduyla beraber çağırarak bu methodumuzu geliştirip kullanabiliriz.
+
+            Console.ReadLine();
+        }
+    }
+    
+    // Normal classlarımız olduğunu düşünelim.
+
+    class Logging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged!");
+        }
+    }
+
+    class Caching : ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cached!");
+        }
+    }
+
+    class Authorize : IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("User checked!");
+        }
+    }
+
+    // Bunların birer interface i olmalı.
+
+    interface ILogging
+    {
+        void Log();
+    }
+
+    interface ICaching
+    {
+        void Cache();
+    }
+
+    interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    // Bunları kullandığımız bir iş sınıfı olsun.
+    class CustomerManager
+    {
+        /*Bunları bu şekilde kullanabiliriz ancak Facade Design a göre bunları ayrı bir class açıp orada new lemek daha doğrudur.
+         * 
+         * private ILogging _logging;
+         private ICaching _caching;
+         private IAuthorize _authorize;
+
+         public CustomerManager(ILogging logging,ICaching caching, IAuthorize authorize)
+         {
+             _logging = logging;
+             _caching = caching;
+             _authorize = authorize;
+         }
+
+         *Facade e göre aşağıdaki gibi yapılır:
+         */
+        CrossCuttingConcernsFacade _concerns;
+        public CustomerManager()
+        {
+            _concerns = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            /* _logging.Log();
+            _caching.Cache();
+            _authorize.CheckUser();
+
+            Üstteki yorum satırlarındaki gibi kullansaydık böyle kullanılacaktılar ancak 
+            Facade Design a göre daha temiz bir koda uyarladığımızda şu şekilde kullanılır:
+            */
+
+            _concerns.Caching.Cache();
+            _concerns.Authorize.CheckUser();
+            _concerns.Logging.Log();
+             
+            Console.WriteLine("Saved!");
+        }
+         
+    }
+
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize(); 
+        }
+    }
+
+
+}
