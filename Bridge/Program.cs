3851e43aa2566942bc28e6b17580412b4a849f89
serifahmetharas <serifahmetharas@gmail.com>
2022-01-25 20:52:13 +0300

    Bridge and Decorator
    Bridge Pattern Design and Decorator Pattern Design are added.

---
+++ Bridge/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Bridge
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager._messageSenderBase = new SMSSender();
+            customerManager.UpdateCustomer();
+            
+            Console.ReadLine();
+        }
+    }
+
+    // Soyut Base sınıfımızı oluşturalım:
+    abstract class MessageSenderBase
+    {
+        // Bir adet sabit Save methodu ve bir adet değişken Send methodu.
+        public void Save()
+        {
+            Console.WriteLine("Message Saved!");
+        }
+
+        public abstract void Send(Body body);
+    }
+
+    // Soyutladığımız sınıfın Concrete sınıfını oluşturalım:
+
+    class Body
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+    
+    // Birden fazla, ihtiyaç dahilinde artan Sender classlarımız olabilir. 
+    // MessageSenderBase li ve Body parametreli method içeren Sender classları:
+    class SMSSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via SMSSender",body.Title);
+           
+        }
+    }
+
+    
+     
+    class EMailSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via EMailSender", body.Title);
+        }
+    }
+
+    //...
+
+    class CustomerManager // Gerekli işlemleri yaptıracak class.
+    {
+
+        public MessageSenderBase _messageSenderBase;
+        public void UpdateCustomer()
+        {   _messageSenderBase.Send(new Body { Title= "About the course"});
+            Console.WriteLine("Customer Updated!");
+        }
+
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Decorator
+{
+    // Elimizde temel bir nesne mevcut iken bu nesneye farklı koşullarda daha farklı anlamlar yüklemek için kullanılan pattern.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personalCar = new PersonalCar { Make = "BMW", Model = "3.20", HirePrice = 2500 };
+
+            SpecialOffer specialOffer = new SpecialOffer(personalCar); // CarBase ister, personalCar verirsin. Ve buna göre özel teklifler vs. gelir.
+            specialOffer.DiscountPercentage = 10;
+
+
+            Console.WriteLine("Concrete: {0}",personalCar.HirePrice); // personalCar normal kiralama ücreti.
+            Console.WriteLine("Special offer: {0}",specialOffer.HirePrice); // personalCar tabanlı üretilen specialOffer kira ücreti
+
+            Console.ReadLine();
+        }
+    }
+
+    // Bir RentACar uygulaması yazdığımızı düşünelim.
+    abstract class CarBase
+    {
+        public abstract string Make { get; set; }
+        public abstract string Model { get; set; }
+        public abstract decimal HirePrice { get; set; }
+
+    }
+
+    class PersonalCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+    }
+
+    class CommercialCar : CarBase
+    {
+        public override string Make { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Model { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override decimal HirePrice { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+    abstract class CarDecoratorBase : CarBase
+    {
+        private CarBase _carBase;
+
+        protected CarDecoratorBase(CarBase carBase)
+        {
+            _carBase = carBase;
+        }
+    }
+
+    class SpecialOffer : CarDecoratorBase
+    {
+        public int DiscountPercentage { get; set; }
+        private readonly CarBase _carBase; // Hangi CarBase i gönderirsek onu kullanalım diye bunu oluştururuz.
+
+        // CarBase ile base alacağımız bir constructor a ihtiyaç duyarız:
+        public SpecialOffer(CarBase carBase) : base(carBase)
+        {
+            _carBase = carBase;
+        } 
+
+        // CarBase property leri direk implemente edilir.
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        // SpecialOffer için HirePrice teklifimizi göndermek için bu HirePrice set edilmelidir:
+        public override decimal HirePrice {
+            get { return _carBase.HirePrice - _carBase.HirePrice * DiscountPercentage / 100; } // Girilen CarBase in indirimli ücretini elde edelim.
+            set { }
+        }
+    }
+
+}
